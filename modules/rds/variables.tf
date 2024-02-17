455e68f8f0d7797b09d2ac139507b6d7a4f7d21c
hongjunpyo <ejhjjp11@naver.com>
2024-02-17 13:52:37 +0900

    feat : module rds variables.tf
    - variables.tf 파일 작성

---
+++ modules/rds/variables.tf
@@ -0,0 +1,23 @@
+variable "db_username" {
+  type    = string
+}
+
+variable "db_password" {
+  type    = string
+}
+
+variable "ap_private_subnet_a" {
+  type    = string
+}
+
+variable "ap_private_subnet_c" {
+  type    = string
+}
+
+variable "db_subnet_group_name" {
+  type    = string
+}
+
+variable "db_vpc_id" {
+  type    = string
+}
