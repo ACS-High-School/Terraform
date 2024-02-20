c0b15b3308d3a0a31413018bf5206098dc6da691
hongjunpyo <ejhjjp11@naver.com>
2024-02-20 17:33:55 +0900

    AHSK-160 feat : module vpc outputs.tf sg 생성에 필요값 추가
    - security-group 생성에 필요한 값 출력

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = module.b3o_vpc.vpc_id
 }
 
+output "public_subnets" {
+  value = module.b3o_vpc.public_subnets
+}
+
 output "private_subnets" {
   value = module.b3o_vpc.private_subnets
 }
@@ -12,6 +16,14 @@ output "database_subnets" {
 
 output "db_subnet_group_name" {
   value = module.b3o_vpc.database_subnet_group_name
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.b3o_vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.b3o_vpc.private_subnets_cidr_blocks
 }
 
 output "database_subnets_cidr_blocks" {
