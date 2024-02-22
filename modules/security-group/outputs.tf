8323fdd75a37bf867933deac0c8780d4993cf3d0
hongjunpyo <ejhjjp11@naver.com>
2024-02-23 03:42:56 +0900

    AHSK-168 feat : module sg outputs.tf(bastion) 값 추가
    - bastion 서버 생성에 필요한 값 outputs.tf에 추가

--- modules/security-group/outputs.tf
+++ modules/security-group/outputs.tf
@@ -1,3 +1,9 @@
+output "bastion_sg_id" {
+  value = aws_security_group.bastion_sg.id
+}
+
+
+
 output "db_sg_id" {
-  value       = aws_security_group.db_sg.id
+  value = aws_security_group.db_sg.id
 }
