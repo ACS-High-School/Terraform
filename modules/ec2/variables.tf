ce9949e9b7a9ba9b8d2674fbdc3cb7481a7deb9f
hongjunpyo <ejhjjp11@naver.com>
2024-02-23 03:38:35 +0900

    AHSK-167 feat : module ec2 variables.tf(jenkins) 작성
    - jenkins 서버에 필요한 variables 파일 작성

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "jenkins_eip_id" {
+  type = string
+}
+
+variable "jenkins_ami" {
+  type = string
+}
+
+variable "jenkins_subnet_id" {
+  type = string
+}
+
+variable "jenkins_vpc_security_group_id" {
+  type = string
+}
+
