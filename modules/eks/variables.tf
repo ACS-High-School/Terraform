9dc0a8175c8e4c71318d1e01a6dfa6a791eebbe8
hongjunpyo <ejhjjp11@naver.com>
2024-02-26 20:49:37 +0900

    AHSK-175 feat : module eks variables.tf 작성
    - eks 생성에 필요한 variables 정의

---
+++ modules/eks/variables.tf
@@ -0,0 +1,20 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "main_region" {
+  type = string
+}
+
