c314ef517a648c53c8bcd48d6dcf971f2bf70f2d
hongjunpyo <ejhjjp11@naver.com>
2024-03-13 23:00:26 +0900

    AHSK-270 feat : module management-tools variables.tf 작성

---
+++ modules/management-tools/variables.tf
@@ -0,0 +1,26 @@
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "cluster_certificate_authority_data" {
+  type = string
+}
+
+variable "grafana_adminPassword" {
+  type = string
+}
+
+variable "grafana_adminUser" {
+  type = string
+}
+variable "main_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
